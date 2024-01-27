6ff0e1c6a915f4dd79fddacc916a4ccd94fe32bc
Krishna <krishnaagrawal2992@gmail.com>
2024-01-28 01:06:36 +0530

    cat api test

--- bitcoinFunctions.js
+++ bitcoinFunctions.js
@@ -2,10 +2,11 @@ const bitcore = require('bitcore-lib')
 const axios = require('axios')
 
 const calculatePublicKey = (privateKey) => {
-    privateKey = bitcore.PrivateKey.fromWIF(privateKey)
-    const publicKey = privateKey.toPublicKey()
-    const compressedPublicKeyString = publicKey.toString()
-    return compressedPublicKeyString
+    // privateKey = bitcore.PrivateKey.fromWIF(privateKey)
+    // const publicKey = privateKey.toPublicKey()
+    // const compressedPublicKeyString = publicKey.toString()
+    // return compressedPublicKeyString
+    return 'test public key🙃'
 }
 
 
